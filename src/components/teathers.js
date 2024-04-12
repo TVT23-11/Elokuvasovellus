c3a25f3fb0053a92742d41ce2088ecd870d78c41
DapiFromKazzak <40441211+DapiFromKazzak@users.noreply.github.com>
2024-04-12 16:49:25 +0300

    Update teathers.js

--- src/components/teathers.js
+++ src/components/teathers.js
@@ -1,4 +1,10 @@
 import React, { useState, useEffect } from 'react';
+
+// Alustetaan päivämäärän tiedot
+const today = new Date();
+const day = String(today.getDate()).padStart(2, '0');
+const month = String(today.getMonth() + 1).padStart(2, '0');
+const year = today.getFullYear();
 const formattedDate = `${year}-${month}-${day}`;
 
 function Theaters() {
