33d723405e5adfba0b28b98ab088c2260231b6d7
DapiFromKazzak <aaro.kallioinen@gmail.com>
2024-04-25 10:59:52 +0300

    Ryhmän esityslistalle voi lisätä näytöksiä ja ne listataan ryhmän omalla sivulla

--- src/components/groupmanagement.js
+++ src/components/groupmanagement.js
@@ -2,15 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { jwtToken } from './authSignals';
 
-const showNewGroupForm = () => {
-  document.getElementById('newGroupFormContainer').style.display = 'block';
-};
-const closeNewGroupForm = () => {
-  document.getElementById('newGroupFormContainer').style.display = 'none';
-  document.getElementById('newGroupForm_groupName').value = '';
-  document.getElementById('newGroupForm_groupDescription').value = '';
-};
-
 const AcceptUserToGroup = (user, group) => {
 
   const data = {
@@ -64,43 +55,6 @@ function isLoggedIn() {
     return false;
   }
   return true;
-};
-
-const AddNewGroup = () => {
-  let name = document.getElementById('newGroupForm_groupName').value;
-  let description = document.getElementById('newGroupForm_groupDescription').value
-  if (name == '') {
-    document.getElementById('newGroupForm_groupName_status').innerHTML = '&#10007; Ryhmälle täyty antaa nimi';
-  } else {
-    const data = {
-      name: name,
-      description: description,
-      token: jwtToken.value
-    };
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify(data)
-    };
-
-    fetch('http://localhost:3001/groups/', requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          document.getElementById('newGroupForm_infoLine').innerHTML = data.error;
-        } else {
-          console.log(data.message);
-          document.getElementById('newGroupForm_infoLine').innerHTML = 'Ryhmä luotiin onnistuneesti';
-
-          //Tyhjennetään lomake
-          document.getElementById('newGroupForm_groupName').value = '';
-          document.getElementById('newGroupForm_groupDescription').value = '';
-
-          document.getElementById('newGroupForm_groupName_status').innerHTML = '';
-          document.getElementById('newGroupForm_groupDescription_status').innerHTML = '';
-        }
-      });
-  }
 };
 
 function PendingJoinRequests() {
@@ -210,38 +164,101 @@ function ManageGroups(){
   
 }
 
-export default function gropupmanagement() {
+
+
+export default function Gropupmanagement() {
+
+  const [activeForm, setActiveForm] = useState(false);
+
+  function HandleForms () {
+    setActiveForm(!activeForm);
+  };
+
+  const AddNewGroup = () => {
+    let name = document.getElementById('newGroupForm_groupName').value;
+    let description = document.getElementById('newGroupForm_groupDescription').value
+    if (name == '') {
+      document.getElementById('newGroupForm_groupName_status').innerHTML = '&#10007; Ryhmälle täyty antaa nimi';
+    } else {
+      const data = {
+        name: name,
+        description: description,
+        token: jwtToken.value
+      };
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify(data)
+      };
+  
+      fetch('http://localhost:3001/groups/', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          if (data.error) {
+            document.getElementById('newGroupForm_infoLine').innerHTML = data.error;
+          } else {
+            console.log(data.message);
+            document.getElementById('newGroupForm_infoLine').innerHTML = 'Ryhmä luotiin onnistuneesti';
+  
+            //Tyhjennetään lomake
+            document.getElementById('newGroupForm_groupName').value = '';
+            document.getElementById('newGroupForm_groupDescription').value = '';
+  
+            document.getElementById('newGroupForm_groupName_status').innerHTML = '';
+            document.getElementById('newGroupForm_groupDescription_status').innerHTML = '';
+
+            //Suljetaan lomake 1,5s kuluttua ryhmän lisäämisestä
+            setTimeout(function (){
+              HandleForms();
+            },1500);
+          }
+        });
+    }
+  };
+
+  function AddNewGroupForm() {
+    return(
+      <div className="newGroupFormContainer" id="newGroupFormContainer">
+        <div className='newGroupFormHeader'>Anna uuden ryhmän tiedot:</div>
+        <div id='newGroupForm_infoLine'>&nbsp;</div>
+        <table>
+          <tbody>
+            <tr>
+              <td>Ryhmän nimi:</td>
+              <td><input type='text' id='newGroupForm_groupName' /><span id="newGroupForm_groupName_status" className='validationStatus'></span></td>
+            </tr>
+            <tr>
+              <td>Ryhmän kuvaus:</td>
+              <td><textarea id='newGroupForm_groupDescription' rows={7} cols={35}></textarea><span id="newGroupForm_groupDescription_status" className='validationStatus'></span></td>
+            </tr>
+            <tr>
+              <td></td>
+              <td><button id='newGroupForm_addNewGroup' onClick={AddNewGroup}>Luo uusi ryhmä</button></td>
+            </tr>
+          </tbody>
+        </table>
+        <button id='closeNewGroupForm' className='closeButton' onClick={() => HandleForms()}>Sulje lomake</button>
+      </div>
+    );
+  }
+
   return (
     <div>
       
       {!isLoggedIn() ? (
         <p>Tämä sivu näkyy vain kirjautuneille käyttäjille. <Link to="/login" className="login-button">Kirjaudu Sisään</Link></p>
       ) : (
-        <div className='groupManagementContainer'>
-          <button className='formButton' onClick={showNewGroupForm}>Luo uusi ryhmä</button>
-          <div className="newGroupFormContainer" id="newGroupFormContainer" style={{ display: 'none' }}>
-            <div className='newGroupFormHeader'>Anna uuden ryhmän tiedot:</div>
-            <div id='newGroupForm_infoLine'>&nbsp;</div>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Ryhmän nimi:</td>
-                  <td><input type='text' id='newGroupForm_groupName' /><span id="newGroupForm_groupName_status" className='validationStatus'></span></td>
-                </tr>
-                <tr>
-                  <td>Ryhmän kuvaus:</td>
-                  <td><textarea id='newGroupForm_groupDescription' rows={7} cols={35}></textarea><span id="newGroupForm_groupDescription_status" className='validationStatus'></span></td>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td><button id='newGroupForm_addNewGroup' onClick={AddNewGroup}>Luo uusi ryhmä</button></td>
-                </tr>
-              </tbody>
-            </table>
-            <button id='closeNewGroupForm' className='closeButton' onClick={closeNewGroupForm}>Sulje lomake</button>
-          </div>
-          <PendingJoinRequests />
-          <ManageGroups />
+          <div className='groupManagementContainer'>
+            <button className='formButton' onClick={() => HandleForms()}>Luo uusi ryhmä</button>
+            {activeForm ? (
+              <AddNewGroupForm />
+            ) : (
+              <div>
+                <PendingJoinRequests />
+                <ManageGroups />
+              </div>
+            )}
+          
         </div>
       )}
     </div>
